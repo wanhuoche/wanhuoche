c0aa4be858cfd321b8204e23e0d45e769c33daa5
Kingtrain <anniecassidyc@gmail.com>
2025-11-11 11:02:03 +0800

    初始化React新代办项目，支持添加/编辑/删除

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+ export default function Table() {
+    const [todos, setTodos] = useState(
+        [{id: 1, text: "开始一个新的计划!", isEditing: false}]
+    );
+    const [newTodo, setNewTodo] = useState("");
+    function handleAdd() {
+        if(newTodo.trim() === "") return;
+        const nextId = todos.length > 0 ? Math.max(...todos.map(t => t.id))+1 : 1;
+        const newItem = {id: nextId, text: newTodo, isEditing: false}
+        setTodos([...todos, newItem])
+        setNewTodo("")
+    }
+
+    function handleEdit(id) {
+        setTodos(todos.map( todo =>
+            todo.id === id ? {...todo, isEditing: true} : todo) 
+        )
+    }
+
+    function handleChange(id, nextText) {
+        setTodos(todos.map(todo => 
+            todo.id === id ? {...todo, text: nextText} : todo
+        ))
+    }
+
+    function handleSave(id) {
+        setTodos(todos.map( todo =>
+            todo.id === id ? {...todo, isEditing: false} : todo) 
+        )
+    }
+
+    function handleDelete(id) {
+        setTodos(todos.filter(todo => todo.id !== id)
+        )
+    }
+
+    return (
+        <>  
+            <h1>KingTrain's react-todoList</h1>
+            <div style={{marginBottom: "1rem" }}>
+                <input 
+                    type="text"
+                    value={newTodo}
+                    onChange={e => setNewTodo(e.target.value)}
+                    onKeyDown={e => e.key === "Enter" ? handleAdd() : null}
+                    placeholder="添加新的代办事项"
+                />
+                <button onClick={handleAdd}>添加</button>
+            </div>
+            <table>
+                <thead>
+                    <tr className="firstrow">
+                        <th>代办事项</th>
+                        <th>操作</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {todos.map(todo =>
+                        (<tr className="row" key={todo.id}>
+                            <td>
+                                {todo.isEditing ? ( 
+                                    <input 
+                                        value={todo.text}
+                                        onChange={e => handleChange(todo.id, e.target.value)}
+                                    />
+                                ) : todo.text}
+                            </td>
+                            <td>
+                                {todo.isEditing ? (
+                                    <button onClick={() =>handleSave(todo.id)}>保存</button>
+                                ) : (
+                                    <div className="button-groups">
+                                        <button onClick={() =>handleEdit(todo.id)}>编辑</button>
+                                        <button onClick={() =>handleDelete(todo.id)}>删除</button>
+                                    </div>
+                                )}
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </>
+    )
 }
-
-export default App;
